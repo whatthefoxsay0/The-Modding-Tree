52d25a10c72737f8ec3a40d8cab01c4455414251
whatthefoxsay0 <99903938+whatthefoxsay0@users.noreply.github.com>
2022-11-13 01:31:31 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -19,7 +19,7 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.1</h3><br>
-		- Endgame: 1e15 bananas.<br>
+		- Endgame: 1e5 bananas.<br>
 		- Added first 3 layers.<br>`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
@@ -59,7 +59,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e15"))
+	return player.points.gte(new Decimal("e5"))
 }
 
 
